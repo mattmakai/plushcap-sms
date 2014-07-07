44f8b6b0c18e556a47906fb9f7e80db5b7c552c0
Matthew Makai <makaimc@gmail.com>
2014-07-07 13:30:03 -0400

    tweaking plushcap main code

--- plushcap/plushcap.py
+++ plushcap/plushcap.py
@@ -2,6 +2,11 @@
 import sys
 import requests
 
+HTTP_OK = 200
+HTTP_FORBIDDEN = 403
+HTTP_NOT_FOUND = 404
+HTTP_INTERNAL_SERVER_ERROR = 500
+
 def contact_url(url):
     """
         Attempts to access the URL specified as a parameter. Returns the
@@ -13,10 +18,21 @@ def contact_url(url):
     except requests.exceptions.ConnectionError:
         return 404, ""
 
+def handle_response(status_code):
+    if status_code == HTTP_OK:
+        return '%s is online and returning 200 OK' % sys.argv[1]
+    elif status_code == HTTP_FORBIDDEN:
+        return 'Unable to access %s due to lack of permissions.' % sys.argv[1]
+    elif status_code == HTTP_NOT_FOUND:
+        return '%s was not found or is not available.' % sys.argv[1]
+    elif status_code == HTTP_INTERNAL_SERVER_ERROR:
+        return '%s is not working properly. Internal server error.' % \
+            sys.argv[1]
+
 
 if __name__=='__main__':
     if len(sys.argv) < 2:
         print("usage: python plushcap.py http://test.url/")
     else:
         status_code, content = contact_url(url=sys.argv[1])
-        print(status_code)
+        print(handle_response(status_code))
