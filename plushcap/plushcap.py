581328b92756d8e7f5e4dc2f7909923514ff00e9
Matthew Makai <makaimc@gmail.com>
2014-07-08 08:46:03 -0400

    fleshing out stubs for notifications when a website goes down

--- plushcap/plushcap.py
+++ plushcap/plushcap.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
-import sys
 import requests
+import sys
 
 CONNECTION_ERROR = -100
 
@@ -23,6 +23,29 @@ def contact_url(url):
         return response.status_code, response.content
     except requests.exceptions.ConnectionError:
         return CONNECTION_ERROR, ""
+
+
+def check_url(url, frequency=60):
+    """
+        Loops in a separate thread checking a given URL. Sends a notification
+        if the website goes down, 500 errors or gives a 404.
+    """
+    while True:
+        time.sleep(60)
+        status_code, content = contact_url(url)
+        if status_code != 200:
+            # alert time
+            pass
+
+
+def monitor(url, frequency=60, twilio_account_sid="", twilio_auth_token=""):
+    """
+        Monitors the given URL. Default frequency is once a minute. Twilio account
+        SID and auth token are required for notifications and will be pulled from
+        the environment variables TWILIO_ACCOUNT_SID and TWILIO_AUTH_TOKEN if not
+        specified.
+    """
+    check_url(url, frequency)
 
 
 if __name__=='__main__':
